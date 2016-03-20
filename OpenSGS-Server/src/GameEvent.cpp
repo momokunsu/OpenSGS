3a4314bcfeb03116f5b67a7ba137a839b68bfc35
momokunsu <601447163@qq.com>
2016-03-20 16:08:13 +0800

    commit

---
+++ OpenSGS-Server/src/GameEvent.cpp
@@ -0,0 +1,257 @@
+#include "GameEvent.h"
+
+std::function<void (uTypeUnion &u, int size)> _endianhandler = nullptr;
+
+void endianChage(uTypeUnion &u, int size)
+{
+	int dsize = size / 2;
+	for(int i = 0; i < dsize; i++)
+	{
+		uchar t = u.charVal[i];
+		u.charVal[i] = u.charVal[size-i-1];
+		u.charVal[size-i-1] = t;
+	}
+}
+
+void endianNotChage(uTypeUnion &u, int size)
+{
+}
+
+uchar GameEvent::m_seribuffer[65536];
+
+GameEvent::GameEvent(eGameEvent ev)
+{
+	m_event_id = ev;
+}
+
+GameEvent::~GameEvent()
+{
+}
+
+void GameEvent::initEndian()
+{
+	uTypeUnion u;
+	u.longVal[0] = 0xFF0000FF;
+	if(u.shortVal[0] == 0x00FF && u.shortVal[1] == 0xFF00)
+		_endianhandler = endianNotChage;
+	else
+		_endianhandler = endianChage;
+}
+
+int GameEvent::getBufferSize(const void * data)
+{
+	return *((ushort*)m_seribuffer);
+}
+
+void GameEvent::writeVal8(uTypeUnion val)
+{
+	*((uchar*)m_cur_ptr) = val.charVal[0];
+	m_cur_size++;
+	m_cur_ptr++;
+}
+
+void GameEvent::writeVal16(uTypeUnion val)
+{
+	_endianhandler(val, 2);
+	*((ushort*)m_cur_ptr) = val.shortVal[0];
+	m_cur_size += 2;
+	m_cur_ptr += 2;
+}
+
+void GameEvent::writeVal32(uTypeUnion val)
+{
+	_endianhandler(val, 4);
+	*((ulong*)m_cur_ptr) = val.longVal[0];
+	m_cur_size += 4;
+	m_cur_ptr += 4;
+}
+
+void GameEvent::writeVal64(uTypeUnion val)
+{
+	_endianhandler(val, 8);
+	*((uint64*)m_cur_ptr) = val.int64Val;
+	m_cur_size += 8;
+	m_cur_ptr += 8;
+}
+
+uTypeUnion GameEvent::readVal8()
+{
+	uTypeUnion val;
+	val.charVal[0] = *((uchar*)m_cur_ptr);
+	m_cur_size ++;
+	m_cur_ptr ++;
+	return val;
+}
+
+uTypeUnion GameEvent::readVal16()
+{
+	uTypeUnion val;
+	val.shortVal[0] = *((ushort*)m_cur_ptr);
+	_endianhandler(val, 2);
+	m_cur_size += 2;
+	m_cur_ptr += 2;
+	return val;
+}
+
+uTypeUnion GameEvent::readVal32()
+{
+	uTypeUnion val;
+	val.longVal[0] = *((ulong*)m_cur_ptr);
+	_endianhandler(val, 4);
+	m_cur_size += 4;
+	m_cur_ptr += 4;
+	return val;
+}
+
+uTypeUnion GameEvent::readVal64()
+{
+	uTypeUnion val;
+	val.int64Val = *((int64*)m_cur_ptr);
+	_endianhandler(val, 8);
+	m_cur_size += 8;
+	m_cur_ptr += 8;
+	return val;
+}
+
+void GameEvent::reSize()
+{
+	*((ushort*)m_seribuffer) = m_cur_size;
+}
+
+
+void * GameEvent::serialize()
+{
+	m_cur_size = 0;
+	m_cur_ptr = m_seribuffer;
+
+	uTypeUnion val;
+
+	val.shortVal[0] = 0;
+	writeVal16(val);
+
+	val.longVal[0] = (ulong)m_event_id;
+	writeVal32(val);
+
+	reSize();
+	return m_seribuffer;
+}
+
+void GameEvent::unserialize(const void * data)
+{
+	m_cur_size = 0;
+	m_cur_ptr = m_seribuffer;
+
+	memcpy(m_seribuffer, data, getBufferSize());
+
+	uTypeUnion val;
+
+	readVal16();//size
+
+	val = readVal32();
+	m_event_id = (eGameEvent)val.longVal[0];
+}
+
+void * EventGetPlayerStatus::serialize()
+{
+	GameEvent::serialize();
+
+	uTypeUnion val;
+
+	val.shortVal[0] = statusMap.size();
+	writeVal16(val);
+	for (auto it : statusMap)
+	{
+		val.charVal[0] = it.first;
+		writeVal8(val);
+		val.charVal[0] = (uchar)it.second;
+		writeVal8(val);
+	}
+
+	reSize();
+	return m_seribuffer;
+}
+
+void EventGetPlayerStatus::unserialize(const void * data)
+{
+	GameEvent::unserialize(data);
+
+	uTypeUnion val;
+
+	val = readVal16();
+	for (int i = 0, size = val.shortVal[0]; i < size; i++)
+	{
+		val = readVal8();
+		auto player = val.charVal[0];
+		val = readVal8();
+		auto status = val.charVal[0];
+		statusMap[player] = (ePlayerStatusType)status;
+	}
+}
+
+void * EventGetCards::serialize()
+{
+	GameEvent::serialize();
+
+	uTypeUnion val;
+
+	val.charVal[0] = playerID;
+	writeVal8(val);
+
+	val.shortVal[0] = cards.size();
+	writeVal16(val);
+	for (auto it : cards)
+	{
+		val.longVal[0] = it;
+		writeVal32(val);
+	}
+
+	reSize();
+	return m_seribuffer;
+}
+
+void EventGetCards::unserialize(const void * data)
+{
+	GameEvent::unserialize(data);
+
+	uTypeUnion val;
+
+	val = readVal8();
+	playerID = val.charVal[0];
+
+	val = readVal16();
+	cards.clear();
+	for (int i = 0, size = val.shortVal[0]; i < size; i++)
+	{
+		val = readVal32();
+		cards.push_back(val.longVal[0]);
+	}
+}
+
+void * EventPhrase::serialize()
+{
+	GameEvent::serialize();
+
+	uTypeUnion val;
+
+	val.charVal[0] = playerID;
+	writeVal8(val);
+
+	val.charVal[0] = (uchar)type;
+	writeVal8(val);
+
+	reSize();
+	return m_seribuffer;
+}
+
+void EventPhrase::unserialize(const void * data)
+{
+	GameEvent::unserialize(data);
+
+	uTypeUnion val;
+
+	val = readVal8();
+	playerID = val.charVal[0];
+
+	val = readVal8();
+	type = (ePhraseType)val.charVal[0];
+}
---
+++ OpenSGS-Server/src/GameEvent.h
@@ -0,0 +1,125 @@
+#pragma once
+
+#include <vector>
+
+#include "def.h"
+#include <map>
+
+//玩家身份
+enum class ePlayerStatusType
+{
+	None = 0,
+	Ruler,
+	Subject,
+	Rebel,
+	Spy
+};
+
+//流程类型
+enum class ePhraseType
+{
+	None = 0,
+	Begin,
+	Ready,
+	Judge,
+	AfterJudge,
+	Draw,
+	AfterDraw,
+	Battle,
+	AfterBattle,
+	Throw,
+	AfterThrow,
+	End,
+};
+
+//玩家事件回调
+enum class eGameEvent
+{
+	None = 0,
+	GameStart,
+	BattleStart,
+	GetPlayerStatus,
+	GetCards,
+	Phrase
+};
+
+class GameEvent
+{
+	public:
+		GameEvent(eGameEvent ev);
+		virtual ~GameEvent();
+
+		void setEvent(eGameEvent ev){ m_event_id = ev; }
+		eGameEvent getEvent() { return m_event_id; }
+
+		virtual void* serialize();
+		virtual void unserialize(const void* data);
+
+		static void initEndian();
+		static int getBufferSize(const void* data);
+
+	protected:
+		void writeVal8(uTypeUnion val);
+		void writeVal16(uTypeUnion val);
+		void writeVal32(uTypeUnion val);
+		void writeVal64(uTypeUnion val);
+
+		uTypeUnion readVal8();
+		uTypeUnion readVal16();
+		uTypeUnion readVal32();
+		uTypeUnion readVal64();
+
+		void reSize();
+
+		static uchar m_seribuffer[65536];
+		uchar *m_cur_ptr;
+		int m_cur_size;
+		eGameEvent m_event_id;
+};
+
+class EventGameStart :public GameEvent
+{
+	public:
+		EventGameStart() :GameEvent(eGameEvent::GameStart) {}
+};
+
+class EventBattleStart :public GameEvent
+{
+	public:
+		EventBattleStart() :GameEvent(eGameEvent::BattleStart) {}
+};
+
+class EventGetPlayerStatus :public GameEvent
+{
+	public:
+		EventGetPlayerStatus() :GameEvent(eGameEvent::GetPlayerStatus) {}
+
+		std::map<uchar, ePlayerStatusType> statusMap;
+
+		void* serialize() override; 
+		void unserialize(const void* data) override;
+};
+
+class EventGetCards :public GameEvent
+{
+	public:
+		EventGetCards() :GameEvent(eGameEvent::GetCards) {}
+
+		uchar playerID;
+		std::vector<int> cards;
+
+		void* serialize() override;
+		void unserialize(const void* data) override;
+};
+
+class EventPhrase :public GameEvent
+{
+	public:
+		EventPhrase() :GameEvent(eGameEvent::Phrase) {}
+
+		uchar playerID;
+		ePhraseType type;
+
+		void* serialize() override;
+		void unserialize(const void* data) override;
+};
